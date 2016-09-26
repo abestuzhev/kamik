fab6bea05ec23fcb943dd2244c81e87a9b3e04df
Aleksey <aleksey.bestuzhev@gmail.com>
2016-09-27 00:44:23 +0300

    changes

--- js/main.js
+++ js/main.js
@@ -8,5 +8,28 @@ $(function(){
 
   $('.c-menu_link').on('click', function(e){
     e.preventDefault();
-  })
+  });
+
+	$(".tabs-menu a").click(function(event) {
+        event.preventDefault();
+        $(this).parent().addClass("current");
+        $(this).parent().siblings().removeClass("current");
+        var tab = $(this).attr("href");
+        $(".tab-content").not(tab).css("display", "none");
+        $(tab).fadeIn();
+    });
+
+	$('.deals_slider').slick({
+		autoplay: false,
+		autoplaySpeed: 5000,
+		dots: true,
+		arrows: true,
+		infinite: true,
+		speed: 500,
+		adaptiveHeight: false,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		variableWidth: true
+	});
+
 });
